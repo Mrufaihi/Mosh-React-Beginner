662ad2af66cbabbc9f9a8faacfb81672c7d805d6
Unranked <malrufaihi@gmail.com>
2024-07-23 07:15:48 +0300

    Mapping & Conditional Rendering
    -Instead of hardcoding, css style will be added later.
    -Any complex code is stored inside const .

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,31 @@
+import { func } from 'prop-types';
+
 //LIST GROUP COMPONENT
 function ListGroup() {
+  let items = ['Tokyo', 'Saudi arabia', 'USA', 'Amesterdam'];
+  const mappedLists = items.map((item) => (
+    <li key={item}> {item} </li> //map each item between <li> element (also add unique key)
+  ));
+
+  const ifEmpty = items.length === 0 && <p>Empty</p>; //.length (true) && <p>:Empty (truthy) | return 2nd
+
+  //#1   if (items.length === 0) {
+  //     return (
+  //       // any html +2 elements need to be wraped with fragment <>
+  //       <>
+  //         <h1>List Group</h1>
+  //         <p>Empty</p>
+  //       </>
+  //     );
+  //   }
+  //#2 (items.length === 0) ? <p>Empty array</p> : null;
+
   return (
-    // class is reserved keyword
-    // <> Represent a Fragment (container like div)
+    // only expression allowed inside
     <>
       <h1>List Group</h1>
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
+      {ifEmpty}
+      <ul className="list-group">{mappedLists}</ul>
     </>
   );
 }
