dd05b4679379741543193e7a9cb764f145004ed2
Unranked <malrufaihi@gmail.com>
2024-08-07 18:10:01 +0300

    useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,29 @@
+import { useState } from 'react';
+
 //imports
-import { MouseEvent } from 'react';
 function ListGroup() {
   const listItem = ['New Londo', 'Orlondo', 'Drake Valley', 'Undead Burge'];
 
+  //let selectedIndex = 0; //this is local to this component and not recognized by React (useState instead)
+  const [selectedIndex, setSelectedIndex] = useState(-1); //to control each item thro its index (-1 init)
+
   const listMapped = listItem.map((item: string, index: number) => {
     return (
-      <li key={index} className="list-group-item" onClick={handleClickChange}>
+      <li
+        key={index}
+        className={
+          selectedIndex === index ? 'list-group-item active' : 'list-group-item'
+        }
+        onClick={() => setSelectedIndex(index)}
+      >
         {item}
       </li>
     );
   });
-  //   type annotaction for onClick is MouseEvent
-  function handleClickChange(event: MouseEvent) {
-    console.log(event);
-  }
-
-  const noItems = listItem.length === 0 && 'No items in list';
 
   return (
     <>
       <h1>List Group</h1>
-      {noItems}
       <ul className="list-group">{listMapped}</ul>
     </>
   );
