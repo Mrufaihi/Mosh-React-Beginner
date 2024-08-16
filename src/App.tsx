3fb40bd0bbdf26edf6c945479bd8141e31ae3e74
Unranked <malrufaihi@gmail.com>
2024-08-16 06:32:27 +0300

    Exercise#2-Updating Array of Objects

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,33 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    // spread is shallow so it wont copy nested obj
-    player: {
-      name: 'Unranked',
-      gender: 'male',
-    },
+  //cart is an obj. that has array of obj, inside one of its properties.
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: 'item-1', quantity: 1 },
+      { id: 2, title: 'item-2', quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: 'Josh' } });
+    setCart({
+      // copy obj first
+      ...cart,
+      items: cart.items.map(
+        (
+          item //access inside prop into array of objs.
+        ) => (item.id === 1 ? { ...item, quantity: 2 } : item) //map the array -> arr items are objs so copy&update only selected ones : else return item no copy
+      ),
+    });
   };
 
   return (
     <div>
-      <button onClick={handleClick}>Change name</button>
-      <p>{game.player.name}</p>
+      <button onClick={handleClick}>add Quantity = 2</button>
+      {cart.items.map((items) =>
+        items.id === 1 ? <p key={items.id}>Quantity: {items.quantity}</p> : null
+      )}
     </div>
   );
 }
