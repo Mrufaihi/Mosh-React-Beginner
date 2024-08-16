81cceb604e9439eba6896faa7926fa9ad6aef291
Unranked <malrufaihi@gmail.com>
2024-08-16 05:29:35 +0300

    Exercise#1-Updating Object State

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 //imports
 import { useState } from 'react';
 import './App.css';
-import Like from './components/Like';
+
 function App() {
-  return <Like onHandleClick={() => console.log('Clicked')} />;
+  const [game, setGame] = useState({
+    id: 1,
+    // spread is shallow so it wont copy nested obj
+    player: {
+      name: 'Unranked',
+      gender: 'male',
+    },
+  });
+
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: 'Josh' } });
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Change name</button>
+      <p>{game.player.name}</p>
+    </div>
+  );
 }
 
 // export App component(fun) to other files
