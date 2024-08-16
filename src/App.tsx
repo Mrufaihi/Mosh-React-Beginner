5ce5755898a50b2ff406d04a958aedd690ce34c3
Unranked <malrufaihi@gmail.com>
2024-08-16 06:34:45 +0300

    Exercise#3-Updating Nested Arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
 //imports
 import { useState } from 'react';
 import './App.css';
+import { set } from 'immer/dist/internal';
 
 function App() {
-  //cart is an obj. that has array of obj, inside one of its properties.
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: 'item-1', quantity: 1 },
-      { id: 2, title: 'item-2', quantity: 1 },
-    ],
+  const [pizza, setPizza] = useState({
+    name: 'Pepporoni Pizza',
+    toppings: ['Pipporoni', 'Tomato'],
   });
 
-  const handleClick = () => {
-    setCart({
-      // copy obj first
-      ...cart,
-      items: cart.items.map(
-        (
-          item //access inside prop into array of objs.
-        ) => (item.id === 1 ? { ...item, quantity: 2 } : item) //map the array -> arr items are objs so copy&update only selected ones : else return item no copy
-      ),
-    });
+  const handleClick = (topping: string) => {
+    setPizza({ ...pizza, toppings: [...pizza.toppings, topping] });
   };
 
   return (
     <div>
-      <button onClick={handleClick}>add Quantity = 2</button>
-      {cart.items.map((items) =>
-        items.id === 1 ? <p key={items.id}>Quantity: {items.quantity}</p> : null
-      )}
+      <button onClick={() => handleClick('cheese')}>add Topping</button>
+      <p>{pizza.toppings.join('-')}</p>
     </div>
   );
 }
