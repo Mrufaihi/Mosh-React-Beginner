d807deb53230bb378b623766dd45d4f53668e78a
Unranked <malrufaihi@gmail.com>
2024-08-21 07:31:25 +0300

    React Hook form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,41 @@
+//React hook FORM library for managing forms
+import { FieldValues, useForm } from 'react-hook-form';
+
 const Form = () => {
+  // obj register & handleSubmite from parent-obj useForm
+  const { register, handleSubmit } = useForm();
+  console.log(register('name')); //just for show
+
+  //   on Form submit can get complex, thus seperate it
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <form>
+    // use handleSubmit(call it here) obj to get data from This form
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* form Name */}
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           NAME:
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          // spread register obj and give it value
+          {...register('name')}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
+      {/* Form Age */}
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           AGE:
         </label>
-        <input id="age" type="text" className="form-control" />
+        <input
+          {...register('age')}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
