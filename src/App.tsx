ff00854b5a715ad0a1e06221a4511c7ca6729f80
Unranked <malrufaihi@gmail.com>
2024-08-27 12:40:15 +0300

    Expense Tracker part3 (Form)
    1 bug with catagory form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 //imports
 import { useState } from 'react';
 import './App.css';
-import Form from './components/Form/Form';
+import Form from './Expense-Tracker/Components/Form';
 import Table from './Expense-Tracker/Components/Table';
+
+// one place for all catagory, export to children
+export const catagorySelect = ['Meats', 'Dairy', 'Breads'];
+
 function App() {
   const [shoppingItems, setShoppingItems] = useState([
     { id: 1, description: 'aaa', catagory: 'Meats', amount: 10 },
@@ -16,13 +20,18 @@ function App() {
   if (shoppingItems.length === 0) return null;
 
   return (
-    <Table
-      items={shoppingItems}
-      // onDelete has id args, when btn is clicked current element id is stored.
-      onDelete={
-        (id) => setShoppingItems(shoppingItems.filter((item) => item.id !== id)) //item.id is in every object
-      }
-    ></Table>
+    <>
+      <Form></Form>
+
+      <Table
+        items={shoppingItems}
+        // onDelete has id args, when btn is clicked current element id is stored.
+        onDelete={
+          (id) =>
+            setShoppingItems(shoppingItems.filter((item) => item.id !== id)) //item.id is in every object
+        }
+      ></Table>
+    </>
   );
 }
 
--- src/Expense-Tracker/Components/Form.tsx
+++ src/Expense-Tracker/Components/Form.tsx
@@ -1,92 +1,112 @@
-// import React from 'react';
+import { z } from 'zod'; //zod the validation library
+import { zodResolver } from '@hookform/resolvers/zod'; //combine react Hooks with Zod (Resolver)
+import { FieldValues, useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { catagorySelect } from '../../App';
 
-// const Form = () => {
-// schema validation in 1 place
-// const schema = z.object({
-//   description: z.string().min(1, { message: 'Enter Description' }).min(3),
-//   amount: z.number({ invalid_type_error: 'Set Amount' }),
-//   catagory: z.string().min(1, { message: 'Enter Catagory' }),
-// });
+const ShoppingCart = () => {
+  // schema validation in 1 place
+  const schema = z.object({
+    description: z.string().min(1, { message: 'Enter Description' }).min(3),
+    amount: z.number({ invalid_type_error: 'Set Amount' }),
+    catagory: z.string().min(1, { message: 'Select Catagory' }), //TODO: fix catagory is empty error
+  });
 
-// // shape of our form taken from schema (hover FormData)
-// type FormData = z.infer<typeof schema>;
+  // shape of our form taken from schema (hover FormData)
+  type FormData = z.infer<typeof schema>;
 
-// //   Form hook is react hook that got some functionalities like (register )
-// const { register, handleSubmit, formState } = useForm<FormData>({
-//   // call resolver and combine form hook with it
-//   resolver: zodResolver(schema), //give resolver our schema
-// }); //form hook
+  //   Form hook is react hook that got some functionalities like (register )
+  const { register, handleSubmit, formState } = useForm<FormData>({
+    // call resolver and combine form hook with it
+    resolver: zodResolver(schema), //give resolver our schema
+  }); //form hook
 
-// //collect data from Form & do whatever
-// const onSubmit = (data: FieldValues) => {
-//   // console.log(data);
-//   setItem(data);
-// };
-//   return (
-//     <div>
-//       {/* search for item */}
-//       <form className="m-4" onSubmit={handleSubmit(onSubmit)}>
-//         <div className="mb-3">
-//           <label className="p-1">Description</label>
-//           <input
-//             {...register('description')}
-//             id="description"
-//             className="w-100"
-//             type="text"
-//           />
-//           {/* if there is an error then display message */}
-//           {formState.errors.description && (
-//             <p className="text-danger">
-//               {formState.errors.description.message}
-//             </p>
-//           )}
-//         </div>
+  const onSubmit = (data: FieldValues) => console.log(data); //collect data from Form & do whatever
 
-//         <div className="mb-3">
-//           <label className="p-1">Catagory</label>
-//           <input
-//             {...register('catagory')}
-//             id="catagory"
-//             className="w-100"
-//             type="text"
-//           />
-//           {/* if there is an error then display message */}
-//           {formState.errors.catagory && (
-//             <p className="text-danger">{formState.errors.catagory.message}</p>
-//           )}
-//         </div>
+  const [item, setItem] = useState({});
 
-//         <div className="mb-3">
-//           <label className="p-1">Amount</label>
-//           <input
-//             {...register('amount', { valueAsNumber: true })}
-//             className="w-100"
-//             type="number"
-//           />
-//           {/* if there is an error then display message */}
-//           {formState.errors.amount && (
-//             <p className="text-danger">{formState.errors.amount.message}</p>
-//           )}
-//         </div>
+  const [catagory, setCatagory] = useState(''); //TODO: this stores value of current catagory
 
-//         <button type="submit" className="btn btn-primary mb-5">
-//           Submit
-//         </button>
-//       </form>
-//       {/* drop down list Filter */}
-//       {/* <div className="m-3">
-//             <label className="p-1" htmlFor="">
-//               Catagory
-//             </label>
-//             <select className="w-100 dropdown" name="" id="">
-//               <option value="none">None</option>
-//               <option value="dairy">Dairy</option>
-//               <option value="meats">Meats</option>
-//               <option value="breads">Breads</option>
-//             </select>
-//           </div> */}
-//     </div>
-//   );
-// };
+  return (
+    <>
+      {/* search for item */}
+      <form className="m-4" onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="Description" className="p-1">
+            Description
+          </label>
+          <input
+            {...register('description')}
+            id="Description"
+            className="w-100"
+            type="text"
+          />
+          {/* if there is an error then display message */}
+          {formState.errors.description && (
+            <p className="text-danger">
+              {formState.errors.description.message}
+            </p>
+          )}
+        </div>
 
-// export default Form;
+        {/* <div className="mb-3">
+          <label className="p-1">Catagory</label>
+          <input
+            {...register('catagory')}
+            id="catagory"
+            className="w-100"
+            type="text"
+          /> */}
+        {/* if there is an error then display message */}
+        {/* {formState.errors.catagory && (
+            <p className="text-danger">{formState.errors.catagory.message}</p>
+          )}
+        </div> */}
+
+        <div>
+          <label className=" p-1">Catagory</label>
+          <select
+            {...register('catagory')} //TODO: this for schema register validation
+            onChange={(e) => setCatagory(e.target.value)} //on change stores current value of catagory
+            className="form-select"
+            name=""
+            id=""
+          >
+            <option value=""></option>
+            {catagorySelect.map((catagory) => (
+              <option key={catagory} value={catagory}>
+                {catagory}
+              </option>
+            ))}
+          </select>
+          {/* if there is an error then display message */}
+          {formState.errors.catagory && (
+            <p className="text-danger">{formState.errors.catagory.message}</p>
+          )}
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="Amount" className="p-1">
+            Amount
+          </label>
+          <input
+            id="Amount"
+            {...register('amount', { valueAsNumber: true })}
+            className="w-100"
+            type="number"
+          />
+          {/* if there is an error then display message */}
+          {formState.errors.amount && (
+            <p className="text-danger">{formState.errors.amount.message}</p>
+          )}
+        </div>
+
+        <button type="submit" className="btn btn-primary mb-5">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default ShoppingCart;
