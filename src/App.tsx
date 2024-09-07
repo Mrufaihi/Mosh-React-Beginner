92037d0a3681df47902523836edb34ee9dd68a23
Unranked <malrufaihi@gmail.com>
2024-09-07 13:32:31 +0300

    Connecting to the Backend (For Reference)

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-//imports
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
-
-//type safety&autoCompletion for what we want to use | then insert inside <>
-interface UserData {
-  id: number;
-  name: string;
-}
+import UserList from './components/UserList/UserList';
 
 function App() {
-  const [users, setUsers] = useState<UserData[]>([]); //to avoid 'never'
-
-  useEffect(() => {
-    //fetch info from server
-    axios
-      .get<UserData[]>('https://jsonplaceholder.typicode.com/users') //may take some time (promise)
-      .then((response) => setUsers(response.data)); //after promised response is received
-  }, []); // cause we setState, must avoid infnite loop
-
-  console.log(users);
-
-  return (
-    <>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name}: {user.id}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <UserList />;
 }
 
 export default App;
